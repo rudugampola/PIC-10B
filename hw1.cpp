f371cca00a83369bab6969ea1f8e6c26271f3f95
Ravindu Udugampola <ravindu365@gmail.com>
2018-01-15 10:47:42 -0800

    Update hw1.cpp

--- hw1.cpp
+++ hw1.cpp
@@ -17,7 +17,7 @@ Function that you are required to implement (stub).
 void compute_overall_score(std::string inputFile, std::string outputFile) {
 
 	std::ifstream input(inputFile);
-	std::vector<int> grade(11);
+	std::vector<int> grade;
 	int value;
 
 	while (input >> value)
