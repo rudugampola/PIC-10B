a495e5493d33fa7fe645587f1728717b50a3ea2c
Ravindu Udugampola <ravindu365@gmail.com>
2018-01-19 12:30:57 -0800

    Update and rename test to hw1-driver.cpp

---
+++ 10B HW Review/hw1-driver.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "hw1.h"
+#include <string>
+
+int main() {
+
+	compute_overall_score("xibalba.txt", "xibalba_overall.txt");
+	//compute_overall_score("file1.txt", "grade1.txt");
+
+	return 0;
+}
